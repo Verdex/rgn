0c6ebd97469040486e4654e862830f8eb65d5ceb
davidlevere@gmail.com <davidlevere@gmail.com>
2026-04-25 00:06:40 -0400

    gitignore and rgn impl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = std::env::args().collect::<Vec<_>>();
+    if args.len() != 3 {
+        panic!("usage: rgn <pattern> <pattern>");
+    }
+    args.remove(0);
+
+    let result = Command::new("rg").arg("-l").arg(args.remove(0)).output().expect("rg failed");
+    let result_str = String::from_utf8(result.stdout).expect("failure converting output");
+    let files = result_str.lines();
+
+    let pattern2 = args.remove(0);
+    for file in files {
+        let result = Command::new("rg").arg(&pattern2).arg(file).output().expect("rg failed");
+        let result_str = String::from_utf8(result.stdout).expect("failure converting output");
+        println!("{}\n{}", file, result_str);
+    }
 }
+
+
